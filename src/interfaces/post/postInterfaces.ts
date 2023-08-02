6d05c37175a71cb81daf4e8b9bb806ee810af446
jasimahmedq2 <jasimahmed90852417@gmail.com>
2023-08-02 20:20:16 +0600

    updated post interfaces

--- src/interfaces/post/postInterfaces.ts
+++ src/interfaces/post/postInterfaces.ts
@@ -11,7 +11,7 @@ export interface IPost {
     profilePic?: string;
   };
   comments: {
-    [x: string]: ReactNode;
+    length: number;
     _id?: string;
     name?: {
       firstName?: string;
